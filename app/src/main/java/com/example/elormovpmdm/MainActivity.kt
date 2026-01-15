2f2f1f55de0712b4474ff18d1f318a2fc623daeb
GorkaVeloso <gorka.velosoag@elorrieta-errekamari.com>
2026-01-15 20:25:55 +0100

    Navegación hecha y fragments creados

--- app/src/main/java/com/example/elormovpmdm/MainActivity.kt
+++ app/src/main/java/com/example/elormovpmdm/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.elormovpmdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,8 @@ class MainActivity : AppCompatActivity() {
     }
     
     private fun initNavigation() {
-    
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomBar.setupWithNavController(navController)
     }
 }
